2acb40b1a116e1f6259cdc804f9337024b484713
khachatryanhovhannes <khachatryanhovhannes02@gmil.com>
2024-02-13 17:12:23 +0400

    Cemmenting form settings

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,21 +1,21 @@
 
-const sendRow = async (data) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    const requestOptions = {
-        method: "post",
-        headers: myHeaders,
-        redirect: "follow",
-        body: JSON.stringify(data)
-    };
+// const sendRow = async (data) => {
+//     const myHeaders = new Headers();
+//     myHeaders.append("Content-Type", "application/json");
+//     const requestOptions = {
+//         method: "post",
+//         headers: myHeaders,
+//         redirect: "follow",
+//         body: JSON.stringify(data)
+//     };
 
-    return await fetch("https://v1.nocodeapi.com/hovhannes02/google_sheets/SknjaUxeNwHaueEe?tabId=feedbacks", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+//     return await fetch("https://v1.nocodeapi.com/hovhannes02/google_sheets/SknjaUxeNwHaueEe?tabId=feedbacks", requestOptions)
+//         .then(response => response.text())
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
 
-} 
+// } 
 
 
 
-export default sendRow
+// export default sendRow
